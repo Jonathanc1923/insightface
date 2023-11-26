afd196688e24c245ff30e84fada415351c7dd841
Jonathan Encina <145049854+Jonathanc1923@users.noreply.github.com>
2023-11-26 00:58:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ to compile
 
 # setup.py
 from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+
 import numpy
 
 setup(
 	name = 'mesh_core_cython',
-    cmdclass={'build_ext': build_ext},
+    cmdclass={'build_ext'},
     ext_modules=[Extension("mesh_core_cython",
                  sources=["mesh_core_cython.pyx", "mesh_core.cpp"],
                  language='c++',
